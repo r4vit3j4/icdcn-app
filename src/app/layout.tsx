830ee92c83a63f7088e9739b93ba110bd2dca750
Ravi Teja <raviteja281003@gmail.com>
2023-08-05 11:14:46 +0530

    Added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={cn(inter.className, "antialiased")}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
