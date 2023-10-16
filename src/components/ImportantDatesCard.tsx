58def45c6eb7c6fd041d9e7bdb64d0d5681b43cf
Ravi Teja <raviteja281003@gmail.com>
2023-10-16 13:41:41 +0530

    Updated dates

--- src/components/ImportantDatesCard.tsx
+++ src/components/ImportantDatesCard.tsx
@@ -1,19 +1,49 @@
-import { importantDates } from "@/data/papers";
-
 const ImportantDatesCard = () => {
   return (
     <div className="p-6 rounded-md border bg-background/50">
       <ul className="flex list-disc ml-6 flex-col gap-2">
-        {importantDates.map(({ title, date }, index) => {
-          return (
-            <li key={index}>
-              <span className="block text-lg font-semibold">{title}</span>
-              <span className="block leading-7 text-muted-foreground font-medium">
-                {date}
-              </span>
-            </li>
-          );
-        })}
+        <li>
+          <span className="block text-lg font-semibold">
+            Opening for Submission
+          </span>
+          <span className="block leading-7 text-muted-foreground font-medium">
+            August 11th, 2023
+          </span>
+        </li>
+        <li>
+          <span className="block text-lg font-semibold">
+            Deadline for Workshop Papers
+          </span>
+          <div className="flex items-center gap-2">
+            <span className="block leading-7 text-muted-foreground font-medium line-through decoration-red-500">
+              September 30th, 2023
+            </span>
+            <span className="leading-7 text-primary font-medium bg-yellow-200 px-2 rounded-lg">
+              Closed
+            </span>
+          </div>
+        </li>
+        <li>
+          <span className="block text-lg font-semibold">
+            Decision on Acceptance/Rejection of the Workshop Papers
+          </span>
+          <div className="flex items-center gap-2">
+            <span className="block leading-7 text-muted-foreground font-medium line-through decoration-red-500">
+              October 15th, 2023
+            </span>
+            <span className="leading-7 text-primary font-medium bg-yellow-200 px-2 rounded-lg">
+              October 20th, 2023
+            </span>
+          </div>
+        </li>
+        <li>
+          <span className="block text-lg font-semibold">
+            Camera-Ready Papers Version Due
+          </span>
+          <span className="block leading-7 text-muted-foreground font-medium">
+            October 31st, 2023
+          </span>
+        </li>
       </ul>
     </div>
   );
